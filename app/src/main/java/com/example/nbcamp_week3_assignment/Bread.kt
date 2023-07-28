31f687fc2aad42010918d782b5b859432e58bd10
yespython <lid8872@gmail.com>
2023-07-28 11:12:38 +0900

    [FEAT] : ⚙️예외처리

--- app/src/main/java/com/example/nbcamp_week3_assignment/Bread.kt
+++ app/src/main/java/com/example/nbcamp_week3_assignment/Bread.kt
@@ -7,10 +7,25 @@ class Bread() : Menu() {
             println("[${num}] ${menu[1].padEnd(6)} ₩ | ${menu[0]}")
             num++
         }
-
-        //예외처리//2
-        var choice = readln()!!.toInt()
-        orderList.add(breadArray[choice - 1][0].toString())
+        while(true){
+            val length = breadArray.size
+            try{
+                var choice = readln()!!.toInt()
+                if(0 < choice && choice <= length) {
+                    orderList.add(breadArray[choice - 1][0].toString())
+                    println("choice : " + choice)
+                    println("bread : " + breadArray[choice - 1][0].toString())
+                    break;
+                } else {
+                    println("1부터 $length 까지 입력 가능합니다.")
+                    continue;
+                }
+            } catch (e:java.lang.NumberFormatException) {
+                println("숫자만 입력 가능합니다.")
+            }
+        }
+//        var choice = readln()!!.toInt()
+//        orderList.add(breadArray[choice - 1][0].toString())
 
     }
 }
